1947233142f845be303ec01ea5833c6a007e449d
sowmya <vnsowmya6@gmail.com>
2022-02-12 15:10:41 +0530

    update

---
+++ 3_Implementation/test/test_calendar.c
@@ -0,0 +1,29 @@
+#include "unity.h"
+#include <calendar_func.h>
+
+void test_LeapYear(void);
+void test_leapYears(void);
+void test_todayOf(void);
+void test_days(void);
+void test_getDayNumber(void);
+
+void setUp()
+{
+}
+
+void tearDown()
+{
+}
+
+int main()
+{
+  UNITY_BEGIN();
+  RUN_TEST(test_LeapYear);
+  RUN_TEST(test_leapYears);
+  RUN_TEST(test_todayOf);
+  RUN_TEST(test_days);
+  RUN_TEST(test_getDayNumber);
+  
+  return UNITY_END();
+}
+
